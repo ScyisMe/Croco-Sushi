541ec9ed8b519cb56f338fd05787c89e6184c1f0
Danylo <daniloit381@gmail.com>
2025-12-17 17:29:25 +0200

    implemented the parallax

--- frontend/components/ParallaxBackground.tsx
+++ frontend/components/ParallaxBackground.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef } from 'react';
 
 export default function ParallaxBackground() {
   const { scrollY } = useScroll();
-  
+
   // Mouse tracking
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
@@ -21,7 +21,7 @@ export default function ParallaxBackground() {
       const { clientX, clientY } = e;
       const { innerWidth, innerHeight } = window;
       // Center origin
-      const x = (clientX / innerWidth) - 0.5; 
+      const x = (clientX / innerWidth) - 0.5;
       const y = (clientY / innerHeight) - 0.5;
       mouseX.set(x);
       mouseY.set(y);
@@ -50,9 +50,9 @@ export default function ParallaxBackground() {
 
   return (
     <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden select-none">
-      {/* Dark overlay to ensure background blends if using screen mode for black bg images */}
-      
-      {/* Avocado - Top Right */}
+      {/* Dark overlay can be handled by parent or here if needed */}
+
+      {/* Ingredient 1 (Avocado-like) - Top Right */}
       <motion.div
         style={{
           x: avoX,
@@ -60,71 +60,71 @@ export default function ParallaxBackground() {
           translateY: avoScrollY,
           rotate: avoRotate,
         }}
-        className="absolute top-20 right-10 opacity-60 w-32 h-32 md:w-48 md:h-48 blur-[2px]"
+        className="absolute top-20 right-10 opacity-80 w-32 h-32 md:w-48 md:h-48 blur-[1px]"
       >
-        <Image 
-          src="/images/parallax/avocado.png" 
-          alt="Avocado" 
-          width={200} 
+        <Image
+          src="/images/parallax/ingredient-1.jpg"
+          alt="Ingredient 1"
+          width={200}
           height={200}
-          className="object-contain mix-blend-screen" // Screen blend because black background
+          className="object-contain" // removed mix-blend-screen as these might be opaque cutouts
         />
       </motion.div>
 
-      {/* Nori - Bottom Left */}
+      {/* Ingredient 2 (Nori-like) - Bottom Left */}
       <motion.div
         style={{
           x: noriX,
           y: noriY,
           translateY: noriScrollY,
         }}
-        className="absolute bottom-40 -left-10 opacity-50 w-40 h-40 md:w-56 md:h-56 blur-[1px]"
+        className="absolute bottom-40 -left-10 opacity-70 w-40 h-40 md:w-56 md:h-56 blur-[2px]"
       >
-        <Image 
-          src="/images/parallax/nori.png" 
-          alt="Nori" 
-          width={250} 
+        <Image
+          src="/images/parallax/ingredient-2.jpg"
+          alt="Ingredient 2"
+          width={250}
           height={250}
-          className="object-contain mix-blend-screen"
+          className="object-contain"
         />
       </motion.div>
 
-      {/* Caviar - Middle Rightish */}
+      {/* Ingredient 3 (Caviar-like) - Middle Rightish */}
       <motion.div
         style={{
           x: caviarX,
           y: caviarY,
           translateY: caviarScrollY,
         }}
-        className="absolute top-1/2 right-[15%] opacity-40 w-24 h-24 blur-[1px]"
+        className="absolute top-1/2 right-[15%] opacity-60 w-24 h-24 blur-[1px]"
       >
-        <Image 
-          src="/images/parallax/caviar.png" 
-          alt="Caviar" 
-          width={150} 
+        <Image
+          src="/images/parallax/ingredient-3.jpg"
+          alt="Ingredient 3"
+          width={150}
           height={150}
-          className="object-contain mix-blend-screen"
+          className="object-contain"
         />
       </motion.div>
-      
-       {/* Extra Avocado - Bottom Right deep */}
-       <motion.div
+
+      {/* Extra Ingredient 1 - Bottom Right deep (Simulated depth) */}
+      <motion.div
         style={{
-            x: noriX, // Reuse nori transform for variance
-            y: avoY,
-            translateY: noriScrollY,
-            rotate: useTransform(scrollY, [0, 1000], [0, -30]),
-          }}
-          className="absolute bottom-10 right-20 opacity-30 w-20 h-20 blur-[3px]"
-        >
-          <Image 
-            src="/images/parallax/avocado.png" 
-            alt="Avocado" 
-            width={150} 
-            height={150}
-            className="object-contain mix-blend-screen"
-          />
-        </motion.div>
+          x: noriX,
+          y: avoY,
+          translateY: noriScrollY,
+          rotate: useTransform(scrollY, [0, 1000], [0, -30]),
+        }}
+        className="absolute bottom-10 right-20 opacity-40 w-20 h-20 blur-[3px]"
+      >
+        <Image
+          src="/images/parallax/ingredient-1.jpg"
+          alt="Ingredient 1"
+          width={150}
+          height={150}
+          className="object-contain"
+        />
+      </motion.div>
     </div>
   );
 }
