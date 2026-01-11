87d98c6582de4735ae214a4bd737ad01227c1083
Mykola <mikolamelnicuk132@gmail.com>
2026-01-11 22:16:49 +0200

    Implementing Full Product Names

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
     const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https:;
@@ -17,7 +16,6 @@ export function middleware(request: NextRequest) {
 `;
 
     const requestHeaders = new Headers(request.headers);
-    requestHeaders.set('x-nonce', nonce);
     requestHeaders.set(
         'Content-Security-Policy',
         // Replace newlines with spaces
